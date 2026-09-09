fb06de73dcf5a9c96870ced4ef8a8341ea9ad328
mae14828 <migi2412@outlook.jp>
2026-09-09 11:58:54 +0900

    どの栄養の計算をしたいか、モードを選択する機能を作った。

--- NutriCalc/NutriCalc.c
+++ NutriCalc/NutriCalc.c
@@ -10,4 +10,12 @@ typedef struct {
 } Nutrition;
 
 int main() {
+	int mode;
+	Nutrition food[15] = { '\0' };
+	puts("指定された栄養成分を計算します（カロリーは5以外すべて出力されます）");
+	printf("1：タンパク質\t2：脂質\n3：糖質\t\t4：タンパク質と脂質と糖質\n5：食塩相当量\t6：カロリー\n7：すべて\n\n");
+	printf("計算する項目を選択してください>>>");
+	scanf("%d", &mode);
+
+	return 0;
 }
