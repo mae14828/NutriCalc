05198e6fd20bc2c19f483973f546bfc24b7e9319
mae14828 <migi2412@outlook.jp>
2026-09-19 15:13:05 +0900

    calc_pfc関数を作り、mode==4の処理を移行した。

--- NutriCalc/NutriCalc.c
+++ NutriCalc/NutriCalc.c
@@ -72,6 +72,33 @@ void calc_carb(void) {
 			printf("\n糖質：%.1lfg\nカロリー：%.1lfkcal\n\n", sum_of_carb, sum_of_calories);
 }
 
+void calc_pfc(void) {
+	Nutrition food[15] = { 0 };
+	double sum_of_protein = 0;
+	double sum_of_fat = 0;
+	double sum_of_carb = 0;
+	double sum_of_calories = 0;
+
+	puts("\n値を実数で「タンパク質,脂質,糖質」のように入力してください。（最大15個まで、どれかがマイナスで終了）");
+	puts("例：2.3,12,23.1");
+
+	for (int i = 0; i < 15; i++) {
+		printf("%dつ目：", i + 1);
+		scanf("%lf,%lf,%lf", &food[i].protein, &food[i].fat, &food[i].carb);
+
+		if (food[i].protein < 0 || food[i].fat < 0 || food[i].carb < 0) {
+			break;
+		}
+
+		food[i].calories = food[i].protein * 4 + food[i].fat * 9 + food[i].carb * 4;
+		sum_of_protein += food[i].protein;
+		sum_of_fat += food[i].fat;
+		sum_of_carb += food[i].carb;
+		sum_of_calories += food[i].calories;
+	}
+	printf("\nタンパク質：%.1lfg\n脂質：%.1lfg\n糖質：%.1lfg\nカロリー：%.1lfkcal\n\n", sum_of_protein, sum_of_fat, sum_of_carb, sum_of_calories);
+}
+
 int main() {
 	int mode;
 
@@ -98,29 +125,7 @@ int main() {
 		}
 
 		else if (mode == 4) {
-			double sum_of_protein = 0;
-			double sum_of_fat = 0;
-			double sum_of_carb = 0;
-			double sum_of_calories = 0;
-
-			puts("\n値を実数で「タンパク質,脂質,糖質」のように入力してください。（最大15個まで、どれかがマイナスで終了）");
-			puts("例：2.3,12,23.1");
-
-			for (int i = 0; i < 15; i++) {
-				printf("%dつ目：", i + 1);
-				scanf("%lf,%lf,%lf", &food[i].protein, &food[i].fat, &food[i].carb);
-
-				if (food[i].protein < 0 || food[i].fat < 0 || food[i].carb < 0) {
-					break;
-				}
-
-				food[i].calories = food[i].protein * 4 + food[i].fat * 9 + food[i].carb * 4;
-				sum_of_protein += food[i].protein;
-				sum_of_fat += food[i].fat;
-				sum_of_carb += food[i].carb;
-				sum_of_calories += food[i].calories;
-			}
-			printf("\nタンパク質：%.1lfg\n脂質：%.1lfg\n糖質：%.1lfg\nカロリー：%.1lfkcal\n\n", sum_of_protein, sum_of_fat, sum_of_carb, sum_of_calories);
+			calc_pfc();
 		}
 
 		else if (mode == 5) {
